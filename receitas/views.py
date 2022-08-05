c97397ccd12eaee5c7f911039912c2a6c5a5690f
Rodrigo <rlvenancio@gmail.com>
2022-08-05 18:17:43 -0300

    Cria template de página
    Cria a página template para as páginas HTML

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-# Para que possamos usar o request
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Receitas</h1> <h2>Bem vindo</h2>')
+    return render(request, 'index.html')
