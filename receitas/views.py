acb3aeb7965f53eaadfdfc010eb2c41371b85f68
Rodrigo <rlvenancio@gmail.com>
2022-08-06 18:48:18 -0300

    Adiciona camado de visualização dos dados do Banco de Dados
    Com essa implementação é possível visualizar no site os dados de receitas constante no Banco de Dados do PostgreeSQL.

--- receitas/views.py
+++ receitas/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 
+# Importa o modelo do banco de dados para buscar as informações no banco.
+from .models import Receita
+
 # Create your views here.
 def index(request):
-    receitas = {
-        1: 'lasanha',
-        2: 'Sopa de Legumes',
-        3: 'Sorvete',
-        4: 'Bolo de Chocolate'
-    }
+    receitas = (Receita.objects.all) # Para o mdelo de receitas retornar todas as receitas existentes
     dados = {
-        'nome_das_receitas': receitas
+        'receitas': receitas
     }
     return render(request, 'index.html', dados)
 
