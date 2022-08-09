f292543a9e04b0b350756981565c27ebe4c03e60
Rodrigo <rlvenancio@gmail.com>
2022-08-09 20:06:06 -0300

    Atualiza refencia do criador da receita
    Essa implementação faz com que apareça o nome do criador da receita, com a implementação da uma função com "__str__"

--- pessoas/models.py
+++ pessoas/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Pessoa(models.Model):
     nome = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
+    # alteração para que quando utilizarmos a refereência, seja apresentado o campo que será refornado com essa função.
+    def __str__(self):
+        return self.nome
