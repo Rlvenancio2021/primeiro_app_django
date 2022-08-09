e99b7c0891a1aff4271c52064436ae927aa4fd2f
Rodrigo <rlvenancio@gmail.com>
2022-08-09 17:34:26 -0300

    Atualiza arquivo Receitas/Admin.py melhora pesquisa na pág "Admin"
    Com essa implementação altera a página "Admin" incluindo campos de de Busca, Filtro e Paginação, facilitando a seleção das informações constantes no Banco de Dados

--- receitas/admin.py
+++ receitas/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 # Importa o modelo de Receitas para o admin
 from .models import Receita
 
+# Classe criada para melhorar a visualização das receitas na página Admin
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo') # Nomes dos campos conforme o bando de dados
+    list_display_links = ('id', 'nome_receita') # Transforma os conteúdos dos campos como links clicáveis.
+    '''
+    No caso dos campos de pesquisa para que funcionem é necessário que sejam lista ou tuplas,
+    por esse motivo é necessário incluir uma "," virgula no final, caso contrário irá apresentar erro.
+    '''
+    search_fields = ('nome_receita',) # Adiciona campo de busca
+    list_filter = ('categoria',) # Adiciona filtro por um campo específico
+    list_per_page = 3 # Adiciona páginação, determina quantos resultadores serão exibidos por página
 
 # Register your models here.
-admin.site.register(Receita)
+admin.site.register(Receita, ListandoReceitas)
