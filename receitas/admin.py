b9640e621ba98f9d04bc76ad9f03137367009426
Rodrigo <rlvenancio@gmail.com>
2022-08-09 20:26:50 -0300

    Função publicada na pág de seleção do Admin
    Essa implementação possibilita editar o campo "Publicado", sem ter a necessidade de abrir todo o campo da receita para edição.

--- receitas/admin.py
+++ receitas/admin.py
@@ -5,7 +5,7 @@ from .models import Receita
 
 # Classe criada para melhorar a visualização das receitas na página Admin
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo') # Nomes dos campos conforme o bando de dados
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'publicada') # Nomes dos campos conforme o bando de dados
     list_display_links = ('id', 'nome_receita') # Transforma os conteúdos dos campos como links clicáveis.
     '''
     No caso dos campos de pesquisa para que funcionem é necessário que sejam lista ou tuplas,
@@ -14,6 +14,7 @@ class ListandoReceitas(admin.ModelAdmin):
     search_fields = ('nome_receita',) # Adiciona campo de busca
     list_filter = ('categoria',) # Adiciona filtro por um campo específico
     list_per_page = 3 # Adiciona páginação, determina quantos resultadores serão exibidos por página
+    list_editable = ('publicada',) # Torna o campo editável na seleção do Admin
 
 # Register your models here.
 admin.site.register(Receita, ListandoReceitas)
