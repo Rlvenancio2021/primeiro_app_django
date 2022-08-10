373c2d63e79422005c682fe80d37f184617cba1e
Rodrigo <rlvenancio@gmail.com>
2022-08-09 21:38:03 -0300

    Função para adiconar uma imagem para cada receita
    Essa implementação cria botão de upload e campo no banco de dados para guardar endereço da imagem criada. Para isso é necessário instalar o pacote "Pillow" para trabalhar com imagem comado - pip install Pillow

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -129,6 +129,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'alurareceita/static')
 ]
+# Rota para os arquivos de media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- receitas/models.py
+++ receitas/models.py
@@ -13,3 +13,4 @@ class Receita(models.Model):
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True) # blank para caso não for possível pegar a informação, pode deixar em branco.
     publicada = models.BooleanField(default=False) # Cria um campo para flag de publicação
+    foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
