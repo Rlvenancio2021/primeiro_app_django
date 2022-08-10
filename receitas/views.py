77f67340ef58d2a5ee9b1eab3b5d4e7e0b532125
Rodrigo <rlvenancio@gmail.com>
2022-08-09 22:50:02 -0300

    Atualiza função busca em todas as páginas

--- receitas/views.py
+++ receitas/views.py
@@ -20,4 +20,15 @@ def receita(request, receita_id):
     return render(request, 'receita.html', receita_a_exibir)
 
 def buscar(request):
-    return render(request, 'buscar.html') # Cria caminha para página de busca. Um método para atender a requisição
+    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar'] # busca correspondente ao conteúdo da variável constante no enderço url apresentado na página busca "http://localhost:8000/busca?buscar=churrasco" nesse exemplo "churrasco"
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+            
+    dados = {
+        'receitas' : lista_receitas
+    }
+    
+    return render(request, 'buscar.html', dados) # Cria caminha para página de busca. Um método para atender a requisição
