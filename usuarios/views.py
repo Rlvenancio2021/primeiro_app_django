0a56ad83601a6188c516768d784c555c0c862d22
Rodrigo <rlvenancio@gmail.com>
2022-08-11 16:44:11 -0300

    Cria efetivamente um usuário e grava na base de dados
    Com essa implementação será possível criar o cadastro de um usuário, e realiza algumas validações dos dados e se o usuário já existe e caso estaja tudo correto gravao novo usuário no banco de dados. Utiliza a biblioteca User do Django.

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
@@ -7,7 +8,21 @@ def cadastro(request):
         email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
-        print(nome, email, senha, senha2)
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O campo e-mail não pode ficar em branco')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('As senhas não são iguais')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists(): # Função que verificar se o usuário já existe. Para este uso é necessário importar a bibliteca User.
+            print('Usuário já cadastrado')
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha) # Cria um objeto para o usuário que deseja cadastrar.
+        user.save() # função que grava o usuário no banco de dados.
+        print('Usuário cadastrado com sucesso!!')
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
