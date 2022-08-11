624374356971908640dc0fabc056f6ea9f2e7b3a
Rodrigo <rlvenancio@gmail.com>
2022-08-11 17:46:37 -0300

    Cria validação do login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def cadastro(request):
@@ -35,7 +36,13 @@ def login(request):
             print('Os campos e-mail e senha não podem ficar em branco')
             return redirect('login')
         print(email, senha)
-        return redirect('dashboard')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get() # Por esse objeto, filtar o e-mail que existe no banco de dados e retorna o nome do usuário.
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso')
+                return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 def logout(request):
