de4c5ee258a6a90aad01a1bc1452bf26cfd286f0
Rodrigo <rlvenancio@gmail.com>
2022-08-11 15:08:09 -0300

    Cria novo app usuários

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pessoas',
     'receitas',
+    'usuarios',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
